8c2f64edc4328d3c082e7441436be3f76b482f8d
Your Name <you@example.com>
2024-05-16 15:47:42 +0700

    Detail

--- app/src/main/java/com/example/mymovies/Activity/DetailActivity.java
+++ app/src/main/java/com/example/mymovies/Activity/DetailActivity.java
@@ -18,22 +18,35 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.example.mymovies.Adapter.ImageListAdapter;
+import com.example.mymovies.Adapter.ListFilmAdapter;
+import com.example.mymovies.ConnectionDB;
+import com.example.mymovies.Domain.Film;
 import com.example.mymovies.Domain.FilmItem;
 import com.example.mymovies.R;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.gson.Gson;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
     private RequestQueue mRequestQueue;
     private StringRequest mStringRequest;
     private ProgressBar progressBar;
-    private TextView titleTxt, movieRateTxt, movieTimeTxt, movieDateTxt, movieSummaryInfo, movieActorsInfo;
+    private TextView titleTxt, movieRateTxt, movieTimeTxt, movieDateTxt, movieSummaryInfo, movieActorsInfo, genreTxt;
     private NestedScrollView scrollView;
     private int idFilm;
     private ShapeableImageView pic1;
-    private ImageView pic2, backImg;
-    private RecyclerView.Adapter adapterImgList;
+    private ImageView pic2, backImg, pic3;
+    private RecyclerView.Adapter adapterImgList, imagesRecyclerView;
     private RecyclerView recyclerView;
+
+   // private List<Film> filmList1;
+   // private ListFilmAdapter listFilmAdapter;
+    Connection connection;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -43,15 +56,12 @@ public class DetailActivity extends AppCompatActivity {
         initView();
         Log.i("id", String.valueOf(idFilm));
 
-        //sendRequest();
-    }
-    private void sendRequest()
-    {
-
+        sendRequest();
     }
 
-//    private void sendRequest() {
-//          //"SELECT * FROM MOVIES WHERE mVI = 0"
+
+    private void sendRequest() {
+//          "SELECT * FROM MOVIES WHERE mId = 0"
 //        mRequestQueue = Volley.newRequestQueue(this);
 //        progressBar.setVisibility(View.VISIBLE);
 //        scrollView.setVisibility(View.GONE);
@@ -78,7 +88,32 @@ public class DetailActivity extends AppCompatActivity {
 //            Log.i("uilover", "onErrorResponse" + error.toString());
 //        });
 //        mRequestQueue.add(mStringRequest);
-//    }
+        ConnectionDB db = new ConnectionDB();
+        connection = db.conclass();
+        TextView name = findViewById(R.id.textViewLabel);
+        if (db != null) {
+            try {
+                String query = "SELECT * FROM Movies WHERE mId =" + idFilm;
+                Statement smt = connection.createStatement();
+                ResultSet set = smt.executeQuery(query);
+                while (set.next()) {
+                    Glide.with(DetailActivity.this).load(set.getString("mImage")).into(pic1);
+                    Glide.with(DetailActivity.this).load(set.getString("mImage1")).into(pic2);
+                    Glide.with(DetailActivity.this).load(set.getString("mImage2")).into(pic3);
+                    titleTxt.setText(set.getString("mName"));
+                    genreTxt.setText(set.getString("mGenre"));
+                    movieRateTxt.setText(String.valueOf(set.getInt("mRating")));
+                    movieTimeTxt.setText(set.getString("mDuration"));
+                    movieDateTxt.setText(set.getString("mDate"));
+                    movieSummaryInfo.setText(set.getString("mDescription"));
+                    movieActorsInfo.setText(set.getString("mActors"));
+
+                }
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
+    }
 
     private void initView() {
         titleTxt = findViewById(R.id.movieNameTxt);
@@ -87,14 +122,16 @@ public class DetailActivity extends AppCompatActivity {
         //1:48:10 - Sua lai FrontEnd
         pic1 = findViewById(R.id.posterNormalImg);
         pic2 = findViewById(R.id.posterBigImg);
+        pic3 = findViewById(R.id.Image2);
         movieRateTxt = findViewById(R.id.movieRateTxt);
+        genreTxt = findViewById(R.id.genreTxt);
         movieTimeTxt = findViewById(R.id.movieTimeTxt);
         movieDateTxt = findViewById(R.id.movieDateTxt);
         movieSummaryInfo = findViewById(R.id.movieSummaryInfo);
         movieActorsInfo = findViewById(R.id.movieActorsInfo);
         backImg = findViewById(R.id.backImg);
-        recyclerView = findViewById(R.id.imagesRecyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        //recyclerView = findViewById(R.id.imagesRecyclerView);
+        //recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
         backImg.setOnClickListener(v -> finish());
 
